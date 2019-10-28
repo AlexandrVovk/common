19725e2d4dc1db760af86ddf049d157f94ddd199
Alexandr.Vovk.V <alexandr.vovk.v@gmail.com>
2019-10-28 21:56:02 +0200

    refactoring flask/main.py

--- flask/main.py
+++ flask/main.py
@@ -7,16 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route("/vegetables")
 def vegetables():
     vegetables_list = ['beans', 'carrot', 'beetroot', 'cucumber']
     return render_template('vegetables.html', list=vegetables_list)
+
 
 @app.route("/fruits")
 def fruits():
     fruits_list = ['melon', 'apple', 'strawberry', 'grape']
     return render_template('fruits.html', list=fruits_list)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
