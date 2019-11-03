8e23e9c32125aacbfc4bae27eee9a693c8cb3b0d
Alexandr.Vovk.V <alexandr.vovk.v@gmail.com>
2019-11-03 10:06:14 +0200

    add files flask_lesson_intro/

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def get_home_page():
     return render_template("home.html", data=get_data())
 
+@app.route('/clock')
+def clock_page():
+    return render_template("clock.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
