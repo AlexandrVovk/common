c44d817e511fb6ccef769d258a50d1be59779432
Alexandr.Vovk.V <alexandr.vovk.v@gmail.com>
2019-12-25 10:59:29 +0200

    add README.md; modify tests.py  in django_intro

--- django_intro/api/tests.py
+++ django_intro/api/tests.py
@@ -4,7 +4,12 @@ from django.test import TestCase
 from django.test import Client
 
 from django.urls import reverse
+# from api.views import POCKEMON_URL
+import requests
 
+POCKEMON_URL = 'https://pokeapi.co/api/v2/'
+response_api_pokemon = requests.get(f'{POCKEMON_URL}/type/3')
+response_api_pokemon_bad = requests.get(f'{POCKEMON_URL}/type/33333')
 
 class StatusViewTests(TestCase):
     client = Client()
@@ -20,3 +25,14 @@ class StatusViewTests(TestCase):
     def test_list_func_view(self):
         response = self.client.get(reverse('list_func'))
         assert response.status_code == HTTPStatus.OK
+
+    def test_pokemon_api_response_dict(self):
+        result = type(dict())
+        actual_result = type(response_api_pokemon.json())
+        self.assertEqual(result, actual_result)
+
+    def test_pokemon_api_response_structure(self):
+        result = ['name', 'url']
+        for item in response_api_pokemon.json()['pokemon']:
+            actual_result = list(item['pokemon'].keys())
+            self.assertEqual(result, actual_result)
